cb3c48787aac42ec737f5d2331392e26d1a340d3
Adam Tibbetts <adamtibbetts@pm.me>
2024-02-07 17:27:57 -0500

    Show existing site scans in frontend

--- main.js
+++ main.js
@@ -1,57 +1,103 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron')
-const urlListGenerator = require('./crawler/urlListGenerator')
-const screenshotTaker = require('./crawler/screenshotTaker')
-const fs = require('fs')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const urlListGenerator = require('./crawler/urlListGenerator');
+const screenshotTaker = require('./crawler/screenshotTaker');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+      contextIsolation: false,
+    },
+  });
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-async function selectDirectory () {
+async function selectDirectory() {
   const directory = dialog.showOpenDialogSync(mainWindow, {
-    properties: ['openDirectory']
-  })
+    properties: ['openDirectory'],
+  });
 
   if (directory) {
-    return directory[0] // Return the selected directory path
+    return directory[0]; // Return the selected directory path
   }
-  return null // Return null if no directory was selected
+  return null; // Return null if no directory was selected
 }
 
-ipcMain.handle('select-directory', selectDirectory)
+ipcMain.handle('select-directory', selectDirectory);
 
-ipcMain.handle('read-directory', async (event, directoryPath) => {
-  const files = await fs.promises.readdir(directoryPath)
-  return files.filter((file) =>
-    file.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)
-  )
-})
+async function readDirectory(directoryPath) {
+  directoryPath = path.resolve(directoryPath);
+
+  if (!fs.existsSync(directoryPath)) {
+    console.log(`Directory does not exist: ${directoryPath}`);
+    return [];
+  }
+
+  let files;
+  try {
+    const readdir = util.promisify(fs.readdir);
+    files = await readdir(directoryPath, { withFileTypes: true });
+  } catch (error) {
+    console.log('An error occurred while reading directory: ', error);
+    throw error;
+  }
+
+  const imageFiles = files
+    .filter((file) => !file.isDirectory())
+    .map((file) => file.name)
+    .filter((name) =>
+      ['.jpg', '.jpeg', '.png', '.gif'].includes(
+        path.extname(name).toLowerCase()
+      )
+    );
+
+    return files;
+}
+
+ipcMain.handle('read-directory', async (event, directoryPath, includeSubdirectories = false) => {
+  try {
+    console.log('receiving read-directory ipcMain.handle', directoryPath);
+    
+    const files = await readDirectory(directoryPath, includeSubdirectories);
+    
+    // Map Dirent objects to file paths
+    const fileList = files.map(file => ({
+      name: file.name,
+      isDirectory: file.isDirectory(),
+      path: path.join(directoryPath, file.name) // Add the full file path
+    }));
+    
+    console.log('read-directory returning files: ', fileList);
+    return fileList;
+  } catch (error) {
+    console.error('An error occurred while reading directory: ', error);
+    throw error;
+  }
+});
+
+
 
 ipcMain.handle('generate-url-list', async (event, settings) => {
-  console.log('ipcMain.handle(generate-url-list)')
-  const urlList = await urlListGenerator.generateUrlList(settings)
-  mainWindow.webContents.send('url-list-generated', urlList)
-  return urlList
-})
+  console.log('ipcMain.handle(generate-url-list)');
+  const urlList = await urlListGenerator.generateUrlList(settings);
+  mainWindow.webContents.send('url-list-generated', urlList);
+  return urlList;
+});
 
 ipcMain.on('start-crawl', async (event, settings, urlList) => {
   try {
-    await screenshotTaker.startCrawl(settings, urlList, mainWindow)
+    await screenshotTaker.startCrawl(settings, urlList, mainWindow);
   } catch (error) {
-    console.error('Error during crawl:', error)
+    console.error('Error during crawl:', error);
     // Optionally, you can send an error message back to the renderer process
-    event.sender.send('crawl-error', error.message)
+    event.sender.send('crawl-error', error.message);
   }
-})
+});
--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
-const { dir, error } = require('console')
-const { ipcRenderer } = require('electron')
-const fs = require('fs')
-const path = require('path')
+const { dir, error } = require('console');
+const { ipcRenderer } = require('electron');
+const fs = require('fs');
+const path = require('path');
 
 document
   .getElementById('generateUrlListButton')
@@ -9,69 +9,132 @@ document
     const settings = {
       baseUrl: document.getElementById('baseUrl').value,
       depth: document.getElementById('depth').value,
-      directory: window.selectedDirectory
-    }
-    console.log('renderer.js got a click from generateUrlListButton')
+      directory: window.selectedDirectory,
+    };
+    console.log('renderer.js got a click from generateUrlListButton');
     ipcRenderer
       .invoke('generate-url-list', settings)
       .then((urlList) => {
         // Assuming urlList is an array of URLs
-        const ulElement = document.getElementById('url-list')
-        ulElement.innerHTML = '' // Clear existing list items if necessary
+        const ulElement = document.getElementById('url-list');
+        ulElement.innerHTML = ''; // Clear existing list items if necessary
 
         // Create a list item for each URL and append it to the ul element
         urlList.forEach((url) => {
-          const li = document.createElement('li')
-          li.textContent = url
-          ulElement.appendChild(li)
-        })
+          const li = document.createElement('li');
+          li.textContent = url;
+          ulElement.appendChild(li);
+        });
       })
       .catch((error) => {
         // Handle the error, possibly by showing an error message to the user
-        console.error('Error generating URL list:', error)
-      })
-  })
+        console.error('Error generating URL list:', error);
+      });
+  });
 
 document.getElementById('settings-form').addEventListener('submit', (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
   const settings = {
     baseUrl: document.getElementById('baseUrl').value,
-    directory: window.selectedDirectory
-  }
+    directory: window.selectedDirectory,
+  };
 
   const urlList = Array.from(document.querySelectorAll('#url-list li')).map(
     (li) => li.textContent
-  )
+  );
 
-  ipcRenderer.send('start-crawl', settings, urlList)
-})
+  ipcRenderer.send('start-crawl', settings, urlList);
+});
 
 ipcRenderer.on('url-list-generated', (event, urlList) => {
-  const list = document.getElementById('url-list')
-  list.innerHTML = urlList.map((url) => `<li>${url}</li>`).join('')
+  const list = document.getElementById('url-list');
+  list.innerHTML = urlList.map((url) => `<li>${url}</li>`).join('');
 
-  document.getElementById('startCrawlButton').disabled = false
-})
+  document.getElementById('startCrawlButton').disabled = false;
+});
 
 ipcRenderer.on('screenshot-saved', (event, screenshotPath) => {
-  const list = document.getElementById('image-list')
-  const listItem = document.createElement('div')
-  const image = document.createElement('img')
+  const list = document.getElementById('image-list');
+  const listItem = document.createElement('div');
+  const image = document.createElement('img');
 
-  image.src = screenshotPath // Set the source of the image to the screenshot path
-  image.alt = 'Screenshot' // Set an alt text for the image
-  image.style.width = '100px' // Optional: Set a width for the image if you want to resize it
-  image.style.height = 'auto' // Optional: Keep the aspect ratio
+  image.src = screenshotPath; // Set the source of the image to the screenshot path
+  image.alt = 'Screenshot'; // Set an alt text for the image
+  image.style.width = '100px'; // Optional: Set a width for the image if you want to resize it
+  image.style.height = 'auto'; // Optional: Keep the aspect ratio
 
-  listItem.appendChild(image) // Append the image to the list item
-  list.appendChild(listItem) // Append the list item to the list
-})
+  listItem.appendChild(image); // Append the image to the list item
+  list.appendChild(listItem); // Append the list item to the list
+});
 
-async function displayImages(siteDirectory) {
-  const siteName = path.basename(siteDirectory);
-  const files = await fs.promises.readdir(siteDirectory);
+document
+  .getElementById('select-directory')
+  .addEventListener('click', async () => {
+    console.log('Get directory handler in renderer.js clicked.');
+    const parentDirectory = await ipcRenderer.invoke('select-directory');
+    if (parentDirectory) {
+      console.log('if (parentDirectory) evaluated true');
+      window.selectedDirectory = parentDirectory; // Store the selected directory path
+      document.getElementById(
+        'selected-directory-display'
+      ).textContent = `Selected Directory: ${parentDirectory}`;
+      displayAllSites(parentDirectory);
+    }
+  });
 
+async function displayAllSites(parentDirectory) {
+  console.log('displayAllSites, ', parentDirectory);
+
+  const sitesContainer = document.getElementById('sites-container');
+  sitesContainer.innerHTML = ''; // Clear previous content
+
+  try {
+    // Get all entries in the parent directory
+    const entries = await ipcRenderer.invoke(
+      'read-directory',
+      parentDirectory,
+      false
+    );
+
+    // Filter out files and keep only directories
+    const siteDirectories = entries.filter((entry) => entry.isDirectory);
+
+    for (const directory of siteDirectories) {
+      const siteName = directory.name;
+      console.log('siteName: ', siteName);
+
+      try {
+        // Read all entries in the directory
+        const directoryEntries = await ipcRenderer.invoke(
+          'read-directory',
+          path.join(parentDirectory, siteName),
+          false
+        );
+
+        // Filter out directories and map to file paths
+        const imageFiles = directoryEntries
+          .filter((entry) => !entry.isDirectory)
+          .map((entry) => path.join(parentDirectory, siteName, entry.name));
+
+        console.log('imageFiles: ', imageFiles);
+
+        if (imageFiles.length > 0) {
+          displayImagesWithHeading(parentDirectory, imageFiles, siteName);
+        } else {
+          console.log(`No images found in ${siteName}`);
+        }
+      } catch (error) {
+        console.log('An error occurred while reading directory: ', error);
+        continue; // Skip to the next iteration of the loop
+      }
+    }
+  } catch (error) {
+    console.log('An error occurred while reading parent directory: ', error);
+  }
+}
+
+function displayImagesWithHeading(siteDirectory, imageFiles, siteName) {
   // Create a new section for this site
   const siteSection = document.createElement('section');
   siteSection.id = `site-${siteName}`;
@@ -83,32 +146,32 @@ async function displayImages(siteDirectory) {
   const imageListDiv = document.createElement('div');
   imageListDiv.className = 'image-list'; // Use a class for styling all image lists
 
-  for (const file of files) {
-    if (!file.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) continue; // Skip non-image files
-
+  for (const filePath of imageFiles) {
     const imgElement = document.createElement('img');
-    imgElement.src = path.join(siteDirectory, file);
+    imgElement.src = `file://${filePath}`; // Set the full file path using the file:// protocol
     imgElement.alt = 'Screenshot';
-    imgElement.style.width = '100px';
-    imgElement.style.height = 'auto';
 
     imageListDiv.appendChild(imgElement);
   }
 
-  siteSection.appendChild(imageListDiv);
+  siteSection.appendChild(imageListDiv); // Append the div to the parent section
+
   document.getElementById('sites-container').appendChild(siteSection);
 }
 
-async function displayAllSites(parentDirectory) {
-  const sitesContainer = document.getElementById('sites-container');
-  sitesContainer.innerHTML = ''; // Clear previous content
+async function displayImages(siteDirectory, imageFiles, siteSection) {
+  console.log('dipslayImages', siteDirectory);
 
-  // Use IPC to read the parent directory
-  const siteDirectories = await ipcRenderer.invoke('read-directory', parentDirectory);
-  for (const siteName of siteDirectories) {
-    const sitePath = path.join(parentDirectory, siteName);
-    // Use IPC to read the site directory
-    const imageFiles = await ipcRenderer.invoke('read-directory', sitePath);
-    displayImages(sitePath, imageFiles);
+  const imageListDiv = document.createElement('div');
+  imageListDiv.className = 'image-list'; // Use a class for styling all image lists
+
+  for (const file of imageFiles) {
+    const imgElement = document.createElement('img');
+    imgElement.src = `file://${path.join(siteDirectory, file)}`;
+    imgElement.alt = 'Screenshot';
+
+    imageListDiv.appendChild(imgElement);
   }
+
+  siteSection.appendChild(imageListDiv); // Append the div to the parent section
 }
